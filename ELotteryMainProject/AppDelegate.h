de2d21c4c479e251a5c230b0675229cb5c911f22
jinqiucheng <jinqiucheng@autohome.com.cn>
2017-03-16 14:18:44 +0800

    Initial Commit

---
+++ ELotteryMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ELotteryMainProject
+//
+//  Created by 金秋成 on 2017/3/16.
+//  Copyright © 2017年 DreamTracer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ELotteryMainProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ELotteryMainProject
+//
+//  Created by 金秋成 on 2017/3/16.
+//  Copyright © 2017年 DreamTracer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
